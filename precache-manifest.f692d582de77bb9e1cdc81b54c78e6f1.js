9843914e44fc4e3c55a475cc1b50abecd85e0d9d
Marzooq13579 <marzooqabs@gmail.com>
2020-06-08 16:13:12 +0530

    Updates

---
+++ precache-manifest.f692d582de77bb9e1cdc81b54c78e6f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38be191a30e4666cb5ae4c3577f5bdf6",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "ed11925f01fc90263410",
+    "url": "/Covid-Tracker/static/css/main.4473becd.chunk.css"
+  },
+  {
+    "revision": "c8d6b152d5b7c1425369",
+    "url": "/Covid-Tracker/static/js/2.74f432e7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-Tracker/static/js/2.74f432e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed11925f01fc90263410",
+    "url": "/Covid-Tracker/static/js/main.32e8739a.chunk.js"
+  },
+  {
+    "revision": "e0f13963ec05851b7d30",
+    "url": "/Covid-Tracker/static/js/runtime-main.b3e33737.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-Tracker/static/media/coronaImg.d7265326.png"
+  }
+]);
